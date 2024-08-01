004dfb67395be473024e6ff81a98ceb9206d52b3
Ashruti Nayak <ashruti.nayak2697@gmail.com>
2024-08-01 16:04:31 +0530

    second few changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,36 +8,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { CreateModelComponent } from './modules/crud/create-model/create-model.component';
 import { CrudComponent } from './modules/crud/crud.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSortModule } from '@angular/material/sort';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { LoaderComponent } from './modules/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+   AppComponent,
    CreateModelComponent,
-   CrudComponent
+   CrudComponent,
+   LoaderComponent
     
   ],
   imports: [
     FormsModule,
-    MatTableModule,
-    MatButtonModule,
-    MatSortModule,
-    MatInputModule,
-    MatDialogModule,
-    MatCardModule,
-    MatSnackBarModule,
+    MaterialModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule
   ],
+  exports:[MaterialModule],
   providers: [],
   entryComponents: [CreateModelComponent],
   bootstrap: [AppComponent]
--- src/app/modules/crud/crud.component.ts
+++ src/app/modules/crud/crud.component.ts
@@ -5,9 +5,11 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { LoaderService } from '../loader/loader.service';
 
 interface TableElement {
   name: string;
+  type: string;
 }
 
 @Component({
@@ -22,7 +24,7 @@ export class CrudComponent implements OnInit {
   show: boolean = false;
   showdbclick: boolean = false;
 
-  constructor(private folderCrudService: CrudServiceService, private router: Router,  private snackBar: MatSnackBar,public dialog: MatDialog) { }
+  constructor(private folderCrudService: CrudServiceService, private router: Router,  private snackBar: MatSnackBar,public dialog: MatDialog,private loaderService: LoaderService) { }
 
   ngOnInit(): void {
     this.list();
@@ -31,15 +33,15 @@ export class CrudComponent implements OnInit {
   // For get all List of given folder
   list() {
     localStorage.setItem('folderName', 'some/other');
-    this.show = true;
+    this.loaderService.show();
     this.showdbclick = false;
     this.folderCrudService.list('some/other').subscribe((data) => {
       if(data){
         this.dataSource = data
-        this.show = false
+        this.loaderService.hide()
       }
       else {
-        this.show = false;
+        this.loaderService.hide();
         this.snackBar.open('Something was wrong.', '', {
           duration: 3000,
           verticalPosition: 'top',
@@ -62,15 +64,18 @@ export class CrudComponent implements OnInit {
   // Delete Folder
   delete(name: string) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      width: '250px',
+      height: '400px',
+      width: '600px',
       data: { message: 'Are you sure you want to delete this item?' }
     });
 
     dialogRef.afterClosed().subscribe((result: any) => {
       if (result) {
         const pathName = localStorage.getItem('folderName') + '/' + name;
+        this.loaderService.show();
         this.folderCrudService.deleteFolder(pathName).subscribe(() => {
           this.list();
+          this.loaderService.hide();
         })
       }
     });
@@ -78,25 +83,27 @@ export class CrudComponent implements OnInit {
 
   // Double click function for enter the folder
   onRowDoubleClick(data: TableElement) {
-    const path = localStorage.getItem('folderName') + '/' + data.name;
-    debugger
-    this.show = true;
-    this. showdbclick = true;
-    localStorage.setItem('folderName', path);
-    this.folderCrudService.list(path).subscribe((data) => {
-      if(data){
-        this.dataSource = data
-        this.show = false
-      }
-      else {
-        this.show = false;
-        this.snackBar.open('Something was wrong.', '', {
-          duration: 3000,
-          verticalPosition: 'top',
-          horizontalPosition: 'right',
-        });
-      }
-    })
+    if(data.type != 'FILE') {
+      const path = localStorage.getItem('folderName') + '/' + data.name;
+      debugger
+      this.loaderService.show();
+      this. showdbclick = true;
+      localStorage.setItem('folderName', path);
+      this.folderCrudService.list(path).subscribe((data) => {
+        if(data){
+          this.dataSource = data
+          this.loaderService.hide()
+        }
+        else {
+          this.loaderService.hide();
+          this.snackBar.open('Something was wrong.', '', {
+            duration: 3000,
+            verticalPosition: 'top',
+            horizontalPosition: 'right',
+          });
+        }
+      })
+    }
   }
 
   // Back to main list screen
